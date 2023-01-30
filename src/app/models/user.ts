19b80a9431fafd5b46e3f5a3cdc77e1ee621d3d3
Hassan <Hassan@DESKTOP-FKBKA82>
2023-01-30 13:17:52 +0200

    add user model

---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+export interface user
+{
+    UserName:string,
+    PasswordChanged:boolean,
+    Name:string,
+    phone:string,
+    JobTitle:string,
+    Address:string,
+    DepartmentId:number,
+    Enabled:boolean
+}
