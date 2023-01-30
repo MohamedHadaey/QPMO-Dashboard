faf6f0347aff52e693b3677c28da927b16a6403a
mohamed hadaey <105833278+MohamedHadaey@users.noreply.github.com>
2023-01-30 11:03:31 +0200

    api_fixs1

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -27,60 +27,37 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup = new FormGroup({
     Username: new FormControl(null, [
       Validators.required,
-      Validators.min(5),
-      Validators.max(20),
+      // Validators.minLength(5),
+      // Validators.maxLength(20),
     ]),
     pass: new FormControl(null, [
       Validators.required,
-      Validators.minLength(5),
-      Validators.maxLength(100),
+      // Validators.minLength(5),
+      // Validators.maxLength(100),
     ]),
     checker: new FormControl(null, [
       Validators.required
     ]),
   });
 
-  // submitLoginForm(loginForm: FormGroup) {
-  //   console.log(loginForm.value);
-  //   // if user delete [disabled]="registerForm.invalid" from html inspect
-  //   if (loginForm.invalid) {
-  //     return;
-  //   } else {
-  //     if (
-  //       loginForm.value.Username == 'admin' &&
-  //       loginForm.value.pass == '12345'
-  //     ) {
-  //       this.toastr.success("Success");
-  //       this._Router.navigate(['/varify-pass']);
-  //     } else {
-  //       this.toastr.error("Failed");
-  //       $('#validate-msg').slideDown();
-  //       setTimeout(this.deleteMsg, 4000);
-  //     }
-  //   }
-  //   this.loginForm.reset();
-  // }
-
   submitLoginForm(loginForm: FormGroup) {
-    console.log(loginForm.value);
     // if user delete [disabled]="registerForm.invalid" from html inspect
     if (loginForm.invalid) {
       return;
     } else {
-      this._AuthService.signIn(this.loginForm.value.Username, this.loginForm.value.pass ).subscribe((res) => {
-        console.log(res)
+      this._AuthService.signIn(this.loginForm.value.Username, this.loginForm.value.pass ).subscribe((response) => {
+        console.log(response)
+        if(response.Code == 200 ) {
+          this.toastr.success("Success");
+          this._Router.navigate(['/varify-pass']);
+          localStorage.setItem('token', response.data.token);
+        }
+        else {
+          this.toastr.error(response.Error_Resp);
+          $('#validate-msg').slideDown();
+          setTimeout(this.deleteMsg, 4000);
+        }
       })
-      // if (
-      //   loginForm.value.Username == 'admin' &&
-      //   loginForm.value.pass == '12345'
-      // ) {
-      //   this.toastr.success("Success");
-      //   this._Router.navigate(['/varify-pass']);
-      // } else {
-      //   this.toastr.error("Failed");
-      //   $('#validate-msg').slideDown();
-      //   setTimeout(this.deleteMsg, 4000);
-      // }
     }
     this.loginForm.reset();
   }
